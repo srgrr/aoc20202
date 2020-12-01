aaf61ea6d84d9e8793578e87e0e7d5a00ab32a12
Sergio Rodriguez <sergio.rodriguez@streamsets.com>
2020-12-01 15:07:53 +0100

    Solved problem 1

---
+++ 1/main.cc
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    ifstream ifs("input.txt");
+    vector< int > nums;
+    int last_num;
+    while(ifs >> last_num) nums.push_back(last_num);
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+    for(int i = 0; i < int(nums.size()); ++i) {
+        int have = nums[i];
+        int need = 2020 - have;
+        if(binary_search(nums.begin(), nums.end(), need)) {
+            cout << have * need << endl;
+            return 0;
+        }
+    }
+}
